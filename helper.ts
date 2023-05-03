c210fd86232662b9f1405a697cf1ce3e029d139e
franckDev21 <82069062+franckDev21@users.noreply.github.com>
2023-05-03 08:49:33 +0100

    Update helper.ts

--- helper.ts
+++ helper.ts
@@ -359,15 +359,21 @@ export const convertSeconds = (seconds: number): string => {
   const hours = Math.floor(seconds / 3600);
   const minutes = Math.floor((seconds % 3600) / 60);
   const remainingSeconds = seconds % 60;
+  let myTime = '';
 
   if (hours > 0) {
-    return `${hours} heure${hours > 1 ? 's' : ''}`;
-  } else if (minutes > 0) {
-    return `${minutes} minute${minutes > 1 ? 's' : ''}`;
-  } else {
-    return `${remainingSeconds} seconde${remainingSeconds > 1 ? 's' : ''}`;
+    myTime += `${hours} heure${hours > 1 ? "s" : ""}`;
+  } 
+  if (minutes > 0) {
+    myTime += ` ${minutes} minute${minutes > 1 ? "s" : ""}`;
   }
-}
+
+  if (remainingSeconds){
+    myTime += ` ${remainingSeconds} seconde${remainingSeconds > 1 ? "s" : ""}`;
+  }
+
+  return myTime;
+};
 
 
 /**
